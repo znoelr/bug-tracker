9133d20e764cb6ef8339a6f8d7e2f537d1b0e773
rennleonz <rennleonz@gmail.com>
2024-02-27 19:20:41 -0500

    Setup router to handle a list of available routes

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,11 +1,21 @@
 import express, { NextFunction, Request, Response } from "express";
 import { NotFoundException } from "./modules/common/exceptions";
+import routerList from './router-list';
+import { RouteConfig } from "./modules/common/types";
 
 const router = express.Router();
 
+const registerRoute = (routeConfig: RouteConfig) => {
+  const {path, router: _router} = routeConfig;
+  router.use(path, _router);
+}
+
+// Register Routes
+routerList.forEach(registerRoute);
+
 // Error 404
-router.use((req: Request, res: Response, next: NextFunction): void => {
-  res.status(404).end('Not found');
+router.use('*', (req: Request, res: Response, next: NextFunction): void => {
+  res.status(404).end('[404] Not found');
 });
 
 // Error 500
