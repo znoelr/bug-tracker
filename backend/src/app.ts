b939f6a33b83d07bf0b7b51f9ad92f8a57e51794
znoelr <rennleonz@gmail.com>
2024-03-02 19:03:32 -0500

    Add body parser middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,8 @@ import appRouter from './router';
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(appRouter);
 
 export default app;
