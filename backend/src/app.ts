faf44c894ab03e5e8b043f8d1c1b1f515f1c0726
znoelr <rennleonz@gmail.com>
2024-03-03 16:01:48 -0500

    Setup dotenv config to load .env file based on APP_ENV

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,13 @@
 import 'reflect-metadata';
+import dotenv from 'dotenv';
+import path from 'path';
 import express, { Express } from "express";
 import appRouter from './router';
+
+const dotenvFileName = `.env${(process.env.APP_ENV && '.' + process.env.APP_ENV) || ''}`;
+dotenv.config({
+  path: path.resolve(process.cwd(), dotenvFileName),
+});
 
 const app: Express = express();
 
