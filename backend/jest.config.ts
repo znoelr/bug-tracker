936bca5c2e5921cc7fdcea1d495148552d732a15
znoelr <rennleonz@gmail.com>
2024-03-04 22:49:31 -0500

    Create and Setup jest setup file

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -5,5 +5,6 @@ const config: Config.InitialOptions = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
 };
 export default config;
---
+++ backend/jest.setup.ts
@@ -0,0 +1,7 @@
+beforeAll(() => {
+  console.log('TODO: Setup mock data');
+});
+
+afterAll(() => {
+  console.log('TODO: Clear DB');
+});
