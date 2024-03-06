7282ac88902e105136080b680a1b6ab015be035a
znoelr <rennleonz@gmail.com>
2024-03-06 02:49:51 -0500

    Update create-task.dto to include validation for constant values

--- backend/src/modules/task/dtos/create-task.dto.ts
+++ backend/src/modules/task/dtos/create-task.dto.ts
@@ -1,4 +1,5 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { TASK_PRIORITY, TASK_SEVERITY, TASK_STATUS, TASK_TYPES } from "../task.constants";
 
 export class CreateTaskDto {
   @IsString()
@@ -11,18 +12,22 @@ export class CreateTaskDto {
   
   @IsString()
   @IsNotEmpty()
+  @IsIn(Object.values(TASK_TYPES))
   type: string;
   
   @IsString()
   @IsNotEmpty()
+  @IsIn(Object.values(TASK_STATUS))
   status: string;
   
   @IsString()
   @IsOptional()
+  @IsIn(Object.values(TASK_SEVERITY))
   severity: string;
   
   @IsString()
   @IsNotEmpty()
+  @IsIn(Object.values(TASK_PRIORITY))
   priority: string;
   
   @IsString()
