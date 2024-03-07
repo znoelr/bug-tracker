42eefd370a54d7d96cbe7d3b8eaccb4d61b78d81
znoelr <rennleonz@gmail.com>
2024-03-06 22:28:09 -0500

    Create middleware to handle params parsing for query filters

--- backend/src/modules/middleware/parse-params-for-query-filter.ts
+++ backend/src/modules/middleware/parse-params-for-query-filter.ts
@@ -1,9 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 import { QueryFilters } from "../common/fetch-objects";
+import { GenericFunction } from "../common/types";
 
-export const parseParamsForQueryFilter = () => async (req: Request, res: Response, next: NextFunction) => {
-  const reqParams = req.params || {};
-  req.queryFilters = new QueryFilters()
-    .setWhere(reqParams);
-  next();
-}
+export const parseParamsForQueryFilter = () =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const reqParams = req.params || {};
+    req.queryFilters = new QueryFilters()
+      .setWhere(reqParams);
+    next();
+  }
+;
+
+export const injectParamsForQueryFilter = (cb: GenericFunction) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    const params = cb(req.params);
+    req.queryFilters = (req.queryFilters || new QueryFilters())
+      .setWhere(params);
+    next();
+  }
+;
