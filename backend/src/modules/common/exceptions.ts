82874b12140ad3e49601e1014a8fe79d00b4562b
znoelr <rennleonz@gmail.com>
2024-03-08 22:30:35 -0500

    Create new Exception class for Bad Request

--- backend/src/modules/common/exceptions.ts
+++ backend/src/modules/common/exceptions.ts
@@ -1,10 +1,28 @@
-import { HTTP_NOT_FOUND } from "./http-status-codes";
+import { HTTP_BAD_REQUEST, HTTP_NOT_FOUND } from "./http-status-codes";
 
-export class NotFoundException extends Error {
+export class BaseHttpException extends Error {
+  public http_code: number = 500;
+
+  constructor(message?: string) {
+    super();
+    this.message = message || 'Something went wrong';
+  }
+}
+
+export class NotFoundException extends BaseHttpException {
   public http_code: number = HTTP_NOT_FOUND;
 
   constructor(message?: string) {
     super();
     this.message = message || 'Not Found';
+  }
+}
+
+export class BadRequestException extends BaseHttpException {
+  public http_code: number = HTTP_BAD_REQUEST;
+
+  constructor(message?: string) {
+    super();
+    this.message = message || 'Bad Request';
   }
 }
