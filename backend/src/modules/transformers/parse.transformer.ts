60e59a52c9c5318a41bc3cec7d2af20de2c6e82c
znoelr <rennleonz@gmail.com>
2024-03-12 22:32:13 -0500

    Create new transformer to ONLY the first entry for sortBy into query options

--- backend/src/modules/transformers/parse.transformer.ts
+++ backend/src/modules/transformers/parse.transformer.ts
@@ -1,7 +1,8 @@
 import { objectReducerForKeys } from "../common/reducers";
+import { QueryOptions } from "../common/types";
 
 export const createComposedKeyFromObjectKeys = (keys: string[]) => (obj: any) => {
-  const composedKeyName = obj.join('_');
+  const composedKeyName = keys.join('_');
   return {
     [composedKeyName]: keys.reduce(objectReducerForKeys(obj), {}),
   };
@@ -9,4 +10,14 @@ export const createComposedKeyFromObjectKeys = (keys: string[]) => (obj: any) =>
 
 export const trimObjectForKeys = (keys: string[]) => (obj: any) => {
   return keys.reduce(objectReducerForKeys(obj), {});
+};
+
+export const trimOnlyFirstEntryOfSortByForField = (fieldName: string) => (queryOptions: QueryOptions) => {
+  const sortBy: any = { createdAt: 'desc' };
+  const sortEntries = Object.entries(queryOptions.sortBy);
+  if (!sortEntries.length) return queryOptions.setSortBy(sortBy);
+  const [[field, sortDirection]] = sortEntries;
+  return queryOptions.setSortBy({
+    [fieldName]: { [field]: sortDirection },
+  });
 };
