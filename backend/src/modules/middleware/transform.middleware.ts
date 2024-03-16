f98928af2b3978f225e508a22f3c8b82cf2b5bf0
znoelr <rennleonz@gmail.com>
2024-03-16 04:39:59 -0500

    Update body transformer middleware function to handle multiple transformer callbacks

--- backend/src/modules/middleware/transform.middleware.ts
+++ backend/src/modules/middleware/transform.middleware.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { GenericFunction } from "../common/types";
 import { catchAsync } from "../common/exception-handlers";
 
-export const transformRequestBody = (cb: GenericFunction) =>
+export const transformRequestBody = (...cbArr: GenericFunction[]) =>
   catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    req.body = await cb(req.body);
+    for (const cb of cbArr) {
+      req.body = await cb(req.body);
+    }
     next();
   })
 ;
