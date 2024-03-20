014705d3ba3745955b9540923ade746a018361df
znoelr <rennleonz@gmail.com>
2024-03-19 19:51:31 -0500

    Refactor app and server files to export app as bootstrap function

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,19 +7,22 @@ import * as config from './config';
 import { httpLogger as appLogger } from './logger';
 import { parseSearchForPagination } from './modules/middleware';
 import { connectRedis } from './modules/redis';
+import { cachePermissionsAccess } from './modules/common/helpers/cache.helpers';
 
-config.init();
+export const bootstrapApp = async (): Promise<Express> => {
+  config.init();
 
-connectRedis();
+  await connectRedis();
+  await cachePermissionsAccess();
 
-const app: Express = express();
+  const app: Express = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(appLogger());
+  // Append pagination
+  app.use(parseSearchForPagination());
+  app.use(appRouter);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(appLogger());
-// Append pagination
-app.use(parseSearchForPagination());
-app.use(appRouter);
-
-export default app;
+  return app;
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,31 @@
 import http from 'http';
-import app from './app';
+import { bootstrapApp } from './app';
 import { disconnect } from './repository/prisma';
 import { disconnectRedis } from './modules/redis';
 
-const httpServer = http.createServer(app);
+(async () => {
+  const app = await bootstrapApp();
+  const httpServer = http.createServer(app);
 
-const server = httpServer.listen(process.env.PORT, () => {
-  console.log(`App listening on port ${process.env.PORT}`);
-});
-
-const closeConnections = (signal: string) => () => {
-  console.log(`[${signal}]`);
-  server.close(async () => {
-    httpServer.closeAllConnections();
-    httpServer.close(async () => {
-      await disconnect();
-      console.log('DB disconnected');
-      await disconnectRedis();
-      console.log('Redis disconnected');
-      console.log('Server closed');
-      process.exit(0);
-    });
+  const server = httpServer.listen(process.env.PORT, () => {
+    console.log(`App listening on port ${process.env.PORT}`);
   });
-}
 
-process.on('SIGINT', closeConnections('SIGINT'));
-process.on('SIGTERM', closeConnections('SIGTERM'));
+  const closeConnections = (signal: string) => () => {
+    console.log(`[${signal}]`);
+    server.close(async () => {
+      httpServer.closeAllConnections();
+      httpServer.close(async () => {
+        await disconnect();
+        console.log('DB disconnected');
+        await disconnectRedis();
+        console.log('Redis disconnected');
+        console.log('Server closed');
+        process.exit(0);
+      });
+    });
+  };
+
+  process.on('SIGINT', closeConnections('SIGINT'));
+  process.on('SIGTERM', closeConnections('SIGTERM'));
+})();
