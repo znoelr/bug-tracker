eb1fe3677996e598a402acbb1bb00371c2f71bbb
znoelr <rennleonz@gmail.com>
2024-03-24 11:14:39 -0500

    Setup jest signin

--- backend/jest.global.d.ts
+++ backend/jest.global.d.ts
@@ -2,4 +2,5 @@ import { DBRecords } from "./src/modules/common/types/db-records";
 
 declare global {
   var records: DBRecords;
+  var signin: (userId: string) => string[];
 }
--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -1,10 +1,22 @@
+import jwt from 'jsonwebtoken';
 import { clear as clearDB, seed as seedDB } from './src/scripts/prisma-seed/seed';
+import { ConfigService } from './src/config/config.service';
+import { JWT_COOKIE_NAME } from './src/modules/common/constants';
+
 
 beforeAll(async () => {
+  console.log('Clearing DB...');
   await clearDB();
+  console.log('Seeding DB...');
   global.records = await seedDB();
 });
 
-afterAll(() => {
-  console.log('TODO: Clear DB');
-});
+
+global.signin = (userId: string): string[] => {
+  const jwtPayload = { sub: userId };
+  const accessToken = jwt.sign(jwtPayload, ConfigService.get<string>('JWT_SECRET'), {
+    expiresIn: Number(ConfigService.get<number>('JWT_EXPIRES_IN_SECONDS') * 10),
+  });
+  const cookie = `${JWT_COOKIE_NAME}=${accessToken}`;
+  return [ cookie ];
+};
