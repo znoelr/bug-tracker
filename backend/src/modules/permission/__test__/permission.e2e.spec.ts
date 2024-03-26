f224e0446e94b4447baca285373404c13f0e2b42
znoelr <rennleonz@gmail.com>
2024-03-26 16:26:55 -0500

    [permission] Update end to end tests

--- backend/src/modules/permission/__test__/permission.e2e.spec.ts
+++ backend/src/modules/permission/__test__/permission.e2e.spec.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { Express } from 'express';
+import { v4 as uuid } from 'uuid';
 import { bootstrapApp } from '../../../app';
 import { fetchPermissions, fetchUser } from '../../../scripts/prisma-seed/fetch-records';
 import { ROLES } from '../../role/role.constants';
@@ -12,6 +13,8 @@ let adminUser: UserDto;
 let cookies: string[];
 let permissions: PermissionDto[];
 
+const urlPrefix = '/permissions';
+
 describe('[PERMISSION]', () => {
   beforeAll(async() => {
     app = await bootstrapApp();
@@ -23,28 +26,151 @@ describe('[PERMISSION]', () => {
     cookies = await global.signin(adminUser.id);
   });
 
-  it('should return a list of permissions', async () => {
-    const res = await request(app)
-      .get('/permissions')
-      .set('Cookie', cookies)
-      .expect(200);
-    expect(res.body).toBeDefined();
-    expect(Array.isArray(res.body)).toBe(true);
+  describe(`GET ${urlPrefix}`, () => {
+    it('should return a list', async () => {
+      const res = await request(app)
+        .get('/permissions')
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(res.body).toBeDefined();
+      expect(Array.isArray(res.body)).toBe(true);
+    });
+
+    it('should return Unauthenticated for missing credentials', async () => {
+      const res = await request(app)
+        .get(urlPrefix)
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('should return a specified permission by ID', async () => {
-    const res = await request(app)
-      .get(`/permissions/${permissions[0].id}`)
-      .set('Cookie', cookies)
-      .expect(200);
-    expect(res.body).toBeDefined();
-    expect(res.body.id).toBe(permissions[0].id);
+  describe(`POST ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .post(urlPrefix)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('should return 404 on invalid permission ID', async () => {
-    await request(app)
-      .get(`/permissions/${Date.now()}`)
-      .set('Cookie', cookies)
-      .expect(404);
+  describe(`PUT ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .put(urlPrefix)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`PATCH ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .patch(urlPrefix)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`DELETE ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .delete(urlPrefix)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`GET ${urlPrefix}/:id`, () => {
+    it('should return one record by ID', async () => {
+      const permission = permissions[0];
+      const res = await request(app)
+        .get(`/permissions/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(res.body).toBeDefined();
+      expect(res.body.id).toBe(permission.id);
+      expect(res.body.name).toBe(permission.name);
+    });
+
+    it('should return Not Found for invalid ID', async () => {
+      const res = await request(app)
+        .get(`/permissions/${Date.now()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Unauthenticated for missing credentials', async () => {
+      const res = await request(app)
+        .get(`${urlPrefix}/${uuid()}`)
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`POST ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .post(`${urlPrefix}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`PATCH ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .patch(`${urlPrefix}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`PUT ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .put(`${urlPrefix}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`DELETE ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .delete(`${urlPrefix}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 });
