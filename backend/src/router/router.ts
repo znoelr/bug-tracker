cf82825b3a12499b054fad5c1532fc59b35f265f
znoelr <rennleonz@gmail.com>
2024-03-26 16:25:37 -0500

    Fix missing json response for Not Found routes

--- backend/src/router/router.ts
+++ backend/src/router/router.ts
@@ -25,7 +25,9 @@ routerList.forEach(registerRoute);
 
 // Error 404
 router.use('*', (req: Request, res: Response, next: NextFunction): void => {
-  res.status(404).end('[404] Not found');
+  res.status(404).json(
+    toJsonError('[404] Not found')
+  );
 });
 
 // Error 500
