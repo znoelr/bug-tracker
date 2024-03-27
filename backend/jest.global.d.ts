a41b3797bf699b96fefc3f52c7d40857b730a861
znoelr <rennleonz@gmail.com>
2024-03-27 16:46:40 -0500

    [jest] Create 'signinNewUser' function to test Forbidden access

--- backend/jest.global.d.ts
+++ backend/jest.global.d.ts
@@ -1,7 +1,9 @@
 declare global {
   var signin: (userId: string) => string[];
+  var signinNewUser: (cookies: string[], app: Express) => Promise<string[]>;
 }
 
 declare namespace globalThis {
   var signin: (userId: string) => string[];
+  var signinNewUser: (cookies: string[], app: Express) => Promise<string[]>;
 }
--- backend/jest.setup.ts
+++ backend/jest.setup.ts
@@ -1,4 +1,7 @@
 import jwt from 'jsonwebtoken';
+import { Express } from 'express';
+import request from 'supertest';
+import { v4 as uuid } from 'uuid';
 import { ConfigService } from './src/config/config.service';
 import { JWT_COOKIE_NAME } from './src/common/constants';
 import { initInfrastructure } from './src/infrastructure';
@@ -14,4 +17,16 @@ global.signin = (userId: string): string[] => {
   });
   const cookie = `${JWT_COOKIE_NAME}=${accessToken}`;
   return [ cookie ];
+};
+
+global.signinNewUser = async (cookies: string[], app: Express): Promise<string[]> => {
+  const { body: newUser } = await request(app)
+    .post('/users')
+    .set('Cookie', cookies)
+    .send({
+      username: `New-user-${uuid()}`,
+      password: 'abcde$12345',
+    })
+    .expect(201);
+  return global.signin(newUser.id);
 };
