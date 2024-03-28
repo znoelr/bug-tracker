b640d2c860798e5075ffb2a9bda2dc3292c01969
znoelr <rennleonz@gmail.com>
2024-03-28 11:36:05 -0500

    [middleware] Create fucntion to respond with HTTP status code

--- backend/src/middleware/end-request.middleware.ts
+++ backend/src/middleware/end-request.middleware.ts
@@ -4,3 +4,9 @@ import { catchAsync } from "../common/exception-handlers";
 export const endBySendJsonFromRequestBody = catchAsync((req: Request, res: Response, next: NextFunction) => {
   res.json(req.body);
 });
+
+export const endBySendStatus = (status: number) =>
+  catchAsync((req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(status);
+  })
+;
