60756898f9f519ddebef86bbf84311cfe0bddc52
znoelr <rennleonz@gmail.com>
2024-03-28 09:38:19 -0500

    [role-permissions] Implement e2e tests

--- backend/src/modules/role/modules/role-permissions/__test__/role-permissions.e2e.spec.ts
+++ backend/src/modules/role/modules/role-permissions/__test__/role-permissions.e2e.spec.ts
@@ -1,5 +1,6 @@
-import { Express } from "express";
 import request from 'supertest';
+import { Express } from "express";
+import { v4 as uuid } from 'uuid';
 import { RoleDto } from '../../../dtos/role.dto';
 import { bootstrapApp } from '../../../../../app';
 import { UserDto } from "../../../../user/dtos/user.dto";
@@ -11,8 +12,29 @@ let app: Express;
 let cookies: string[];
 let adminUser: UserDto;
 let adminRole: RoleDto;
-let testRole: RoleDto;
 let adminPermissions: PermissionDto[];
+
+const urlPrefix = `/roles/:id/permissions`;
+
+const getAdminPermissions = async (cookies: string[]) => {
+  const { body } = await request(app)
+    .get(`/roles/${adminRole.id}/permissions`)
+    .set('Cookie', cookies)
+    .expect(200);
+  return body;
+}
+
+const createRole = async () => {
+  const { body } = await request(app)
+    .post('/roles')
+    .set('Cookie', cookies)
+    .send({
+      name: `TEST_ROLE - ${Date.now()} - ${Math.random()}`,
+      description: 'Descriptio for TEST_ROLE',
+    })
+    .expect(201);
+  return body;
+};
 
 describe('[ROLE_PERMISSIONS]', () => {
   beforeAll(async () => {
@@ -20,83 +42,326 @@ describe('[ROLE_PERMISSIONS]', () => {
     adminUser = await fetchUser({username: ROLES.ADMIN.toLowerCase()});
     adminRole = await fetchRole({name: ROLES.ADMIN});
     cookies = global.signin(adminUser.id);
-
-    const { body: pBody } = await request(app)
-      .get(`/roles/${adminRole.id}/permissions`)
-      .set('Cookie', cookies)
-      .expect(200);
-      adminPermissions = pBody;
-
-    const { body: rBody } = await request(app)
-      .post('/roles')
-      .set('Cookie', cookies)
-      .send({
-        name: `TEST_ROLE - ${Date.now()} - ${Math.random()}`,
-        description: 'Descriptio for TEST_ROLE',
-      })
-      .expect(201);
-    testRole = rBody;
+    adminPermissions = await getAdminPermissions(cookies);
   });
 
-  it('should list all permissions for a specified role', async () => {
-    const response = await request(app)
-      .get(`/roles/${adminRole.id}/permissions`)
-      .set('Cookie', cookies)
-      .expect(200);
-    expect(response.body).toBeDefined();
-    expect(Array.isArray(response.body)).toBe(true);
+  describe(`GET ${urlPrefix}`, () => {
+    it('should return a list', async () => {
+      const response = await request(app)
+        .get(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(response.body).toBeDefined();
+      expect(Array.isArray(response.body)).toBe(true);
+    });
+
+    it('should return Unauthorized for missing credentials', async () => {
+      const res = await request(app)
+        .get(urlPrefix.replace(':id', adminRole.id))
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Forbidden for lack of access', async () => {
+      const newUserCookies = await global.signinNewUser(cookies, app);
+      const res = await request(app)
+        .get(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', newUserCookies)
+        .expect(403);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('should return a specified permission for a specified role', async () => {
-    const response = await request(app)
-      .get(`/roles/${adminRole.id}/permissions/${adminPermissions[0].id}`)
-      .set('Cookie', cookies)
-      .expect(200);
-    expect(response.body).toBeDefined();
-    expect(response.body.id).toBe(adminPermissions[0].id);
+  describe(`POST ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .post(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('should create a permission for a specified role', async () => {
-    const response = await request(app)
-      .put(`/roles/${testRole.id}/permissions/${adminPermissions[0].id}`)
-      .set('Cookie', cookies)
-      .expect(201);
-    expect(response.body).toBeDefined();
-    expect(response.body.id).toBe(adminPermissions[0].id);
+  describe(`PUT ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .put(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('should delete a permission for a specified role', async () => {
-    const response = await request(app)
-      .put(`/roles/${testRole.id}/permissions/${adminPermissions[1].id}`)
-      .set('Cookie', cookies)
-      .expect(201);
-    expect(response.body).toBeDefined();
-    expect(response.body.id).toBe(adminPermissions[1].id);
-
-    await request(app)
-      .delete(`/roles/${testRole.id}/permissions/${adminPermissions[1].id}`)
-      .set('Cookie', cookies)
-      .expect(204);
+  describe(`PATCH ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .patch(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('throws Bad Request on create, for existing role permissions', async () => {
-    await request(app)
-      .put(`/roles/${adminRole.id}/permissions/${adminPermissions[0].id}`)
-      .set('Cookie', cookies)
-      .expect(400);
+  describe(`DELETE ${urlPrefix}`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const res = await request(app)
+        .delete(urlPrefix.replace(':id', adminRole.id))
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('throws Not Found on create, for bad permission ID', async () => {
-    await request(app)
-      .put(`/roles/${adminRole.id}/permissions/${Date.now()}`)
-      .set('Cookie', cookies)
-      .expect(404);
+  describe(`GET ${urlPrefix}/:id`, () => {
+    it('should return one record by ID', async () => {
+      const permission = adminPermissions[0];
+      const response = await request(app)
+        .get(`${urlPrefix.replace(':id', adminRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(response.body).toBeDefined();
+      expect(response.body.id).toBe(permission.id);
+    });
+
+    it('should return Not Found for invalid ID', async () => {
+      const res = await request(app)
+        .get(`${urlPrefix.replace(':id', adminRole.id)}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Unauthorized for missing credentials', async () => {
+      const res = await request(app)
+        .get(`${urlPrefix.replace(':id', adminRole.id)}/${uuid()}`)
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Forbidden for lack of access', async () => {
+      const newUserCookies = await global.signinNewUser(cookies, app);
+      const res = await request(app)
+        .get(`${urlPrefix.replace(':id', adminRole.id)}/${uuid()}`)
+        .set('Cookie', newUserCookies)
+        .expect(403);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 
-  it('throws Not Found on delete, for bad permission ID', async () => {
-    await request(app)
-      .delete(`/roles/${adminRole.id}/permissions/${Date.now()}`)
-      .set('Cookie', cookies)
-      .expect(404);
+  describe(`POST ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const permission = adminPermissions[0];
+      const res = await request(app)
+        .post(`${urlPrefix.replace(':id', adminRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`PUT ${urlPrefix}/:id`, () => {
+    it('should create a new record', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(201);
+      expect(res.body).toBeDefined();
+      expect(res.body.id).toBe(permission.id);
+      expect(res.body.name).toBe(permission.name);
+      expect(res.body.action).toBe(permission.action);
+      expect(res.body.resource).toBe(permission.resource);
+
+      const { body: newPermission } = await request(app)
+        .get(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(200);
+      expect(newPermission).toBeDefined();
+      expect(newPermission.id).toBe(permission.id);
+    });
+
+    it('should return Bad Request for existing record', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(201);
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(400);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Not Found for invalid ID', async () => {
+      const newRole = await createRole();
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Not Found for invalid role ID', async () => {
+      const permission = adminPermissions[0];
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', uuid())}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Not Found for invalid IDs', async () => {
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', uuid())}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it.todo('should update cached permissions');
+
+    it.todo('should NOT update cached permissions on error response');
+
+    it('should return Unauthorized for missing credentials', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Forbidden for lack of access', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      const newUserCookies = await global.signinNewUser(cookies, app);
+      const res = await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', newUserCookies)
+        .expect(403);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`PATCH ${urlPrefix}/:id`, () => {
+    it('should return Not Found for unimplemented method', async () => {
+      const newRole = await createRole();
+      const res = await request(app)
+        .patch(`${urlPrefix.replace(':id', newRole.id)}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+  });
+
+  describe(`DELETE ${urlPrefix}/:id`, () => {
+    it('should delete one record by ID', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(201);
+      await request(app)
+        .delete(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(204);
+    });
+
+    it('should return Not Found for invalid ID', async () => {
+      const newRole = await createRole();
+      const res = await request(app)
+        .delete(`${urlPrefix.replace(':id', newRole.id)}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Not Found for invalid role ID', async () => {
+      const permission = adminPermissions[0];
+      const newRole = await createRole();
+      await request(app)
+        .put(`${urlPrefix.replace(':id', newRole.id)}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(201);
+      const res = await request(app)
+        .delete(`${urlPrefix.replace(':id', uuid())}/${permission.id}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Not Found for invalid IDs', async () => {
+      const res = await request(app)
+        .delete(`${urlPrefix.replace(':id', uuid())}/${uuid()}`)
+        .set('Cookie', cookies)
+        .expect(404);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Unauthorized for missing credentials', async () => {
+      const newRole = await createRole();
+      const res = await request(app)
+        .delete(`${urlPrefix.replace(':id', newRole.id)}/${uuid()}`)
+        .expect(401);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
+
+    it('should return Forbidden for lack of access', async () => {
+      const newUserCookies = await global.signinNewUser(cookies, app);
+      const newRole = await createRole();
+      const res = await request(app)
+        .delete(`${urlPrefix.replace(':id', newRole.id)}/${uuid()}`)
+        .set('Cookie', newUserCookies)
+        .expect(403);
+      expect(res.body).toBeDefined();
+      expect(res.body.errors).toBeDefined();
+      expect(res.body.errors.message).toBeDefined();
+    });
   });
 });
