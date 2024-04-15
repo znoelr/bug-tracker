c706c9481d7f09a53d156f6bd05182c0ec2452c4
znoelr <rennleonz@gmail.com>
2024-04-15 13:51:15 -0500

    [role-permissions] Implement middleware to handle reset cached user roles

--- backend/src/modules/role/modules/role-permissions/middlewares/reset-cached-permissions.middleware.ts
+++ backend/src/modules/role/modules/role-permissions/middlewares/reset-cached-permissions.middleware.ts
@@ -2,11 +2,23 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../../../../common/exception-handlers";
 import { client } from "../../../../../infrastructure/redis";
 import { cachePermissionsAccess } from "../../../../../common/helpers/cache.helpers";
+import { getUserRolesIds, saveUserRoles } from "../../../../auth/middlewares/restrict-to.middleware";
 
 export const resetCachedPermissions = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   try {
     await client.flushAll();
     await cachePermissionsAccess();
+  }
+  finally {
+    next();
+  }
+});
+
+export const resetCachedUserRoles = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.params.userId;
+    const userRoles = await getUserRolesIds(userId);
+    await saveUserRoles(userId, userRoles);
   }
   finally {
     next();
