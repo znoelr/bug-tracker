f3a5d937df598e335bddbf7bec5676f8e285b038
znoelr <rennleonz@gmail.com>
2024-04-25 12:49:03 -0500

    Setup swagger config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import 'reflect-metadata';
 import './globals';
+import swaggerUi from 'swagger-ui-express';
 import express, { Express } from "express";
 import cookieParser from 'cookie-parser';
 import appRouter from './router/router';
 import * as config from './config';
 import { httpLogger as appLogger } from './logger';
 import { parseSearchForPagination } from './middleware';
+
+const swaggerDocument = require('./swagger.json');
 
 export const bootstrapApp = async (): Promise<Express> => {
   config.init();
@@ -17,6 +20,7 @@ export const bootstrapApp = async (): Promise<Express> => {
   app.use(appLogger);
   // Append pagination
   app.use(parseSearchForPagination());
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(appRouter);
 
   return app;
